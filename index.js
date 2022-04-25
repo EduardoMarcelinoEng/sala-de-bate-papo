0330dd817745bdab850352b0e372f99efab3d031
Eduardo Marcelino <eduardo.marcelino.eng@gmail.com>
2022-04-25 16:21:31 -0300

    Primeiro commit

---
+++ index.js
@@ -0,0 +1,78 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const { Server } = require('socket.io');
+const path = require('path');
+const port = 3000;
+const moment = require('moment-timezone');
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, '/public')));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'));
+
+app.get('/', (req, res)=>{
+    try {
+        return res.render('pages/nickname', {
+            title: 'Registro'
+        });
+    } catch (error) {
+        return res.status(500).json(error.message);
+    }
+});
+
+app.get('/chat', (req, res)=>{
+    try {
+        if(!req.query.nickname) return res.status(301).redirect('/');
+        return res.render('pages/chat', {
+            title: 'Chat'
+        });
+    } catch (error) {
+        return res.status(500).json(error.message);
+    }
+});
+
+const server = http.createServer(app);
+const io = new Server(server);
+
+let messages = []
+
+io.on('connection', (socket) => {
+
+    socket.broadcast.emit("new-participant", {
+        nickname: socket.handshake.query.nickname
+    });
+
+    socket.on("new-message", data=>{
+
+        let date = moment().tz('America/Sao_Paulo');
+
+        let objJSON = {
+            nickname: socket.handshake.query.nickname,
+            message: data,
+            createdAt: date
+        }
+
+        messages.push(objJSON);
+
+        socket.broadcast.emit("new-message", objJSON);
+        
+    });
+
+    socket.on("get-messages", data=>{
+        data(messages);
+    });
+
+    socket.on("disconnect", () => {
+        socket.broadcast.emit("participant-exited", {
+            nickname: socket.handshake.query.nickname
+        });
+    });
+    
+});
+
+setInterval(()=>{
+    messages = messages.filter(message=>moment(message.createdAt).tz('America/Sao_Paulo').add(1, 'hours') > moment().tz('America/Sao_Paulo'));
+}, 1000);
+
+server.listen(port, ()=>console.log(`Aplicação rodando na porta ${port}`));
